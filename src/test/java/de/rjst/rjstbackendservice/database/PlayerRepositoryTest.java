b61452e01ecfe8f3b61dfab1a119587a1774c6a0
RStenzhorn <business.rst@outlook.de>
2024-09-28 11:37:27 +0200

    init

--- src/test/java/de/rjst/rjstbackendservice/database/PlayerRepositoryTest.java
+++ src/test/java/de/rjst/rjstbackendservice/database/PlayerRepositoryTest.java
@@ -15,27 +15,26 @@ import java.util.List;
 import static de.rjst.rjstbackendservice.TestUtil.ANY_STRING;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest()
-//@ActiveProfiles("tests")
-//class PlayerRepositoryTest {
-//
-//    @Autowired
-//    private PlayerRepository playerRepository;
-//
-//    @BeforeEach
-//    void setUp() {
-//        PlayerEntity player = new PlayerEntity();
-//        player.setName(ANY_STRING);
-//        player.setPassword(ANY_STRING);
-//        playerRepository.save(player);
-//    }
-//
-//    @Test
-//    void getAll() {
-//        List<PlayerEntity> result = playerRepository.findAll();
-//
-//        assertThat(result.size()).isEqualTo(1);
-//    }
-//
-//}
+@SpringBootTest
+@ActiveProfiles("tests")
+class PlayerRepositoryTest {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @BeforeEach
+    void setUp() {
+        PlayerEntity player = new PlayerEntity();
+        player.setName(ANY_STRING);
+        player.setPassword(ANY_STRING);
+        playerRepository.save(player);
+    }
+
+    @Test
+    void getAll() {
+        List<PlayerEntity> result = playerRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+}
