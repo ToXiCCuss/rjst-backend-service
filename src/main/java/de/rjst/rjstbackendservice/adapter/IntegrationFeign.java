f356924f774c81174665d26c08d7be80169670db
RStenzhorn <business.rst@outlook.de>
2024-12-19 12:12:44 +0100

    Feign

--- src/main/java/de/rjst/rjstbackendservice/adapter/IntegrationFeign.java
+++ src/main/java/de/rjst/rjstbackendservice/adapter/IntegrationFeign.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "integration", url = "rjst-integration.vpn.rjst.de")
+@FeignClient(name = "integration", url = "https://rjst-integration.vpn.rjst.de")
 public interface IntegrationFeign {
 
     @PostMapping("/data")
