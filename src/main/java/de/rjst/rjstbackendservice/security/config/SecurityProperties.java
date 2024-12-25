c2d5f4a2542ef4be53a3622cc20fa90cf4c74b23
RStenzhorn <business.rst@outlook.de>
2024-12-25 18:10:06 +0100

    Monitoring

--- src/main/java/de/rjst/rjstbackendservice/security/config/SecurityProperties.java
+++ src/main/java/de/rjst/rjstbackendservice/security/config/SecurityProperties.java
@@ -1,6 +1,5 @@
 package de.rjst.rjstbackendservice.security.config;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.convert.Delimiter;
