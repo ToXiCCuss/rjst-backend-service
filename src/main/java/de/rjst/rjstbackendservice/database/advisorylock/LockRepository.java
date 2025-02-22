886917d220bbf11aba66dc80145ea763ac5cf8b7
RStenzhorn <business.rst@outlook.de>
2025-02-23 00:31:24 +0100

    AdvisoryLock

--- src/main/java/de/rjst/rjstbackendservice/database/advisorylock/LockRepository.java
+++ src/main/java/de/rjst/rjstbackendservice/database/advisorylock/LockRepository.java
@@ -9,11 +9,16 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class LockRepository {
 
+    private static final String LOCK_QUERY = "SELECT pg_try_advisory_xact_lock(:lockId)";
+    private static final String APP_ID = "appId";
+    private static final String LOCK_ID = "lockID";
+
     private final EntityManager entityManager;
 
+
     public boolean tryAdvisoryLock(final long lockId) {
-        final Query query = entityManager.createNativeQuery("SELECT pg_try_advisory_xact_lock(:lockId)");
-        query.setParameter("lockId", lockId);
+        final Query query = entityManager.createNativeQuery(LOCK_QUERY);
+        query.setParameter(LOCK_ID, lockId);
         return (Boolean) query.getSingleResult();
     }
 
