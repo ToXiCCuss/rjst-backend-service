4529159ef1e43ecfe016c6bc8becb2bafdc2f3ad
RStenzhorn <business.rst@outlook.de>
2025-03-06 21:39:03 +0100

    Remove specific version from springHelm in Jenkinsfile

--- src/test/java/de/rjst/rjstbackendservice/controller/EnvIT.java
+++ src/test/java/de/rjst/rjstbackendservice/controller/EnvIT.java
@@ -6,6 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class EnvIT {
 
+    static {
+        System.setProperty("DOCKER_HOST", "tcp://vpn.rjst.de:2375");
+    }
+
     @Test
     void contextLoads() {
         String getenv = System.getenv("DOCKER_HOST");
