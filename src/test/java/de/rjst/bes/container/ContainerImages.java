c2d95f3351dd13e166f1f9e9f2a43740754fc0cf
RStenzhorn <business.rst@outlook.de>
2025-05-30 22:48:11 +0200

    upgrade

--- src/test/java/de/rjst/bes/container/ContainerImages.java
+++ src/test/java/de/rjst/bes/container/ContainerImages.java
@@ -7,9 +7,9 @@ import org.testcontainers.utility.DockerImageName;
 public class ContainerImages {
 
     public static final DockerImageName MOCK_SERVER = DockerImageName
-            .parse("harbor.vpn.rjst.de/mockserver/mockserver")
+            .parse("harbor.vpn.rjst.de/docker.io/mockserver/mockserver")
             .withTag("mockserver-" + MockServerClient.class.getPackage().getImplementationVersion());
 
-    public static final DockerImageName POSTGRESQL = DockerImageName.parse("harbor.vpn.rjst.de/postgres:17.5");
+    public static final DockerImageName POSTGRESQL = DockerImageName.parse("harbor.vpn.rjst.de/docker.io/postgres:17.5");
 
 }
