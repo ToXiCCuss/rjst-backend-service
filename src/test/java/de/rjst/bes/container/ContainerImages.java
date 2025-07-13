830d2b3a9a9a4cd51f4874868620db5b4e962f33
RStenzhorn <business.rst@outlook.de>
2025-07-14 00:07:32 +0200

    Revert "init"
    This reverts commit 6950da1fe3b00e2050cebb1cfb0c04810b22fef4.

---
+++ src/test/java/de/rjst/bes/container/ContainerImages.java
@@ -0,0 +1,18 @@
+package de.rjst.bes.container;
+
+import org.mockserver.client.MockServerClient;
+import org.testcontainers.utility.DockerImageName;
+
+
+public class ContainerImages {
+
+    public static final DockerImageName MOCK_SERVER = DockerImageName
+        .parse("harbor.vpn.rjst.de/docker.io/mockserver/mockserver")
+        .withTag("mockserver-" + MockServerClient.class.getPackage()
+                                                       .getImplementationVersion())
+        .asCompatibleSubstituteFor("mockserver/mockserver");
+
+    public static final DockerImageName POSTGRESQL = DockerImageName.parse("harbor.vpn.rjst.de/docker.io/postgres:17.5")
+                                                                    .asCompatibleSubstituteFor("postgres");
+
+}
